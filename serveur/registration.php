2bfc434f78b6cf43ced3c1975d4f4ea90cde72bf
Loanns <lstum@guardiaschool.fr>
2024-12-16 10:39:00 +0100

    Update registration.php

--- serveur/registration.php
+++ serveur/registration.php
@@ -1,25 +1,11 @@
 <?php
     include("index.php");
+
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
-        <h2></h2>
-        username:<br>
-        <input type="text" name="username"><br>
-        password:<br>
-        <input type="password" name="password"><br>
-        <input type="submit" name="submit" value="register">
-    </form>
-</body>
-</html>
+<?php
+    include("index.html");
+?>
+
 <?php
 // modifier quand la table sql sera créé
 
